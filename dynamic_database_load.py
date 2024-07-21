36def72c7aec20b5156aaf6e663a036bc60c7abf
leezzangmin <ckdals12345678@gmail.com>
2024-07-21 17:30:41 +0900

    test

--- dynamic_database_load.py
+++ dynamic_database_load.py
@@ -3,10 +3,17 @@ import aws_clients
 
 def find_all_cluster_info():
     manager = aws_clients.AwsClientManager()
-    rds_client = boto3.client('rds', region_name='ap-northeast-2')
-    response = rds_client.describe_db_clusters()
-    print(response)
-    pass
+    
+    rds_clients = manager.find_all_rds_clients()
+    for account_id in rds_clients:
+        rds_client = rds_clients[account_id]
+        describe_db_clusters_response = rds_client.describe_db_clusters()
+        print(describe_db_clusters_response)
+        for i in describe_db_clusters_response:
+            print(i)
+            print(type(i),'ttttt')
+        print('tpe',type(describe_db_clusters_response))
+
 
 
     # public Map<String, List<DBCluster>> findAllClusterInfo() {
@@ -24,5 +31,21 @@ def find_all_cluster_info():
     #     log.info("clusters: {}", dbClusters);
     #     return dbClusters;
     # }
-print(1)
 
+def isValidDbCluster(describe_db_clusters_response):
+    # describe_db_clusters_response.
+    pass
+
+    #     private DescribeDbClustersResponse findValidClusters(DescribeDbClustersResponse describeDbClustersResponse) {
+    #     DescribeDbClustersResponse clustersResponse = DescribeDbClustersResponse.builder()
+    #             .dbClusters(describeDbClustersResponse.dbClusters().stream()
+    #                     .filter(cluster -> cluster.status().equals("available"))
+    #                     .filter(cluster -> !cluster.tagList().contains(TagStandard.standardTagKeyNames))
+    #                     .filter(cluster -> isCurrentEnvHasValidTag(cluster.tagList()))
+    #                     .collect(Collectors.toList()))
+    #             .build();
+    #     return clustersResponse;
+    # }
+    
+
+find_all_cluster_info()
